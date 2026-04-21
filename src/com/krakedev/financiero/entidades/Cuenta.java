599bedbfabc6f2e3518fdff1bc5c39e07132cd8a
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-04-20 22:21:29 -0500

    BN: Se crea la clase Cuenta con sus atributos y métodos

---
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -0,0 +1,60 @@
+package com.krakedev.financiero.entidades;
+
+public class Cuenta {
+
+	private String id;
+	private double saldoActual;
+	private String tipo;
+	private Cliente propietario;
+
+	public Cuenta(String id) {
+		super();
+		this.id = id;
+		this.saldoActual = 0;
+		this.tipo = "A";
+		this.propietario = new Cliente();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getSaldoActual() {
+		return saldoActual;
+	}
+
+	public void setSaldoActual(double saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Cliente getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(Cliente propietario) {
+		this.propietario = propietario;
+	}
+
+	public void imprimir() {
+
+		String mensaje = "Id: " + id 
+				+ "\nSaldo Actual: " + saldoActual 
+				+ "\nTipo: " + tipo 
+				+ "\n---CLIENTE---";
+		System.out.println(mensaje);
+		propietario.imprimir();
+	}
+
+}
