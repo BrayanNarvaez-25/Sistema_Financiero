3aeecac87f3070f8425d2c187a771fe9e5a0a76a
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-04-20 22:24:46 -0500

    BN: Se crean test de cuenta y cliente

---
+++ src/com/krakedev/financiero/entidades/test/TestCuenta.java
@@ -0,0 +1,13 @@
+package com.krakedev.financiero.entidades.test;
+
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class TestCuenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cuenta c1 = new Cuenta("1");
+		c1.imprimir();
+	}
+
+}
